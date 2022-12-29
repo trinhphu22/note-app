d62d1241eafae35f6de7e68ae68542b1b7e585ea
Forly <forly@MacBook-Pro-cua-Forly.local>
2022-12-29 16:33:05 +0700

    set layout

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Routes from "../routes/Routers";
+
+
+const Layout = () => {
+  return (
+    <div className='container'>
+      <Routes />
+    </div>
+  )
+}
+
+export default Layout
